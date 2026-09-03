77848d290737e1fc04415b2dbc01d213b0b68b6f
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2026-09-03 12:10:52 -0400

    Add more images

--- src/lib/components/Taskbar.svelte
+++ src/lib/components/Taskbar.svelte
@@ -6,6 +6,10 @@ Taskbar in desktop GUI
 
 <script lang="ts">
 import menuImg from "$lib/assets/kali-panel-menu.svg";
+import desktopImg from "$lib/assets/user-blue-desktop.svg";
+import fileManagerImg from "$lib/assets/system-file-manager.svg";
+import textEditorImg from "$lib/assets/accessories-text-editor.svg";
+import firefoxImg from "$lib/assets/firefox.svg";
 
 const openMainMenu = () => {};
 </script>
@@ -20,6 +24,34 @@ const openMainMenu = () => {};
       />
     </button>
     <div class="taskbar-divider"></div>
+    <button aria-label="Open desktop">
+      <enhanced:img
+        class="taskbar-img"
+        src={desktopImg}
+        alt="Blue desktop icon"
+      />
+    </button>
+    <button aria-label="Open file manager">
+      <enhanced:img
+        class="taskbar-img"
+        src={fileManagerImg}
+        alt="File manager icon"
+      />
+    </button>
+    <button aria-label="Open text editor">
+      <enhanced:img
+        class="taskbar-img"
+        src={firefoxImg}
+        alt="Firefox icon"
+      />
+    </button>
+    <button aria-label="Open text editor">
+      <enhanced:img
+        class="taskbar-img"
+        src={textEditorImg}
+        alt="Document with pencil enclosed in a red circle in the bottom right"
+      />
+    </button>
   </div>
   <div></div>
   <div></div>
